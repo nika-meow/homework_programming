b036af42dfc631052faa58e2284dd6c71fd11190
Popova Veronika <nikapopova098@gmail.com>
2025-04-08 20:56:06 +0300

    feat: add a function to search, insert, delete a list item and add a list deletion function

--- 6_1.cpp
+++ 6_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <limits>
 using namespace std;
 
 struct list {
@@ -22,7 +22,7 @@ void push (list *&h, list *&t, int x){ //вставка элемента в ко
     }
     t = r; //r теперь хвост
 }
-
+    
 void print (list *h, list *t){ //печать элементов списка
     list *p = h; //укзатель на голову
     while (p){  //пока не дошли до конца списка
@@ -30,7 +30,60 @@ void print (list *h, list *t){ //печать элементов списка
         p = p->next; //переход к следующему элементу
     }
 }
+
+list *find ( list *h, list *t, int x){ //поиск элемента списка
+    list *p = h; //укзатель на голову
+    while (p){ //пока не дошли до конца списка
+        if (p->inf == x) break; // если нашли, прекращаем цикл
+        p = p->next; //переход к следующему элементу
+    }
+    return p; //возвращаем указатель
+}
     
+void insert_after(list *&h, list *&t, list *r, int y){//вставляем после r
+    list *p=new list; //создаем новый элемент
+    p->inf=y;
+    if(r==t){ //если вставляем после хвоста
+        p->next = NULL; //вставляемый эл-т - последний
+        p->prev = r; //вставляем после r
+        r->next = p;
+        t = p; //теперь хвост - p
+    }
+    else{ //вставляем в середину списка
+        r->next->prev = p; //для следующего за r эл-та предыдущий - p
+        p->next = r->next; //следующий за p - следующий за r
+        p->prev = r; //p вставляем после r
+        r->next = p;
+    }
+}
+
+void del_node(list *&h, list *&t, list *r){ //удаляем после r
+    if(r == h && r == t) //единственный элемент списка
+        h = t = NULL;
+    else if(r == h){ //удаляем голову списка
+        h = h->next; //сдвигаем голову
+        h->prev = NULL;
+    }
+    
+    else if (r == t){ //удаляем хвост списка
+        t = t->prev; //сдвигаем хвост
+        t->next = NULL;
+    }
+    else{
+        r->next->prev = r->prev; //для след-го от r пред. стан-ся r->prev
+    }
+    delete r; //удаляем r
+}
+
+void del_list(list *&h, list *&t){ //удаляем список
+    while(h){ //пока список не пуст
+        list *p = h; //указательнаголову
+        h = h->next; //переносим голову
+        h->prev = NULL; //обнуляем
+        delete p; //удаляем p
+    }
+}
+
 int main(){
 
     return 0;
