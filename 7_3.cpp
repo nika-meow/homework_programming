60a5cdd98e5d7240dc8b45118a6df28701afc186
Popova Veronika <nikapopova098@gmail.com>
2025-04-16 22:31:51 +0300

    feat: min search function , next search function and node delete function

--- 7_3.cpp
+++ 7_3.cpp
@@ -23,7 +23,7 @@ void preorder (tree *tr){ // прямой обход (К-Л-П)
         cout << tr->inf << " "; //корень
         preorder(tr->left); //левое
         preorder(tr->right); //правое
-    } 
+    }
 }
 
 void postorder (tree *tr){ // обратный обход (Л-П-К)
@@ -33,13 +33,13 @@ void postorder (tree *tr){ // обратный обход (Л-П-К)
         cout << tr->inf << " "; //корень
     }
 }
-    
+
 void inorder (tree *tr){ // симметричный обход (Л-К-П)
     if (tr){
         inorder(tr->left); //левое
         cout << tr->inf << " "; //корень
         inorder(tr->right); //правое
-    }  
+    }
 }
 
 void insert(tree *&tr, int x) { //вставка
@@ -75,6 +75,80 @@ tree *find(tree *tr, int x){//поиск
         return find(tr->left, x);//ищем по левой ветке
     else
         return find(tr->right, x);//ищем по правой ветке
+}
+
+tree *Min(tree *tr){//поиск min
+    if (!tr->left) return tr;//нет левого ребенка
+    else return Min(tr->left);//идем по левой ветке до конца
+}
+
+tree *Next(tree*tr, int x){//поиск следующего
+    tree* n = find(tr, x);
+    if (n->right)//если есть правый ребенок
+        return Min(n->right);//min по правой ветке
+    tree *y = n->parent; //родитель
+    while (y && n == y->right){//пока не дошли до корня или узел - правый ребенок
+        n = y;//идем вверх по дереву
+        y = y->parent;
+    }
+    return y;//возвращаем родител
+}
+
+void Delete(tree *&tr, tree *v){//удаление узла
+    tree *p = v->parent;
+    if (!p) tr = NULL; //дерево содержит один узел
+    else if (!v->left && !v->right){//если нет детей
+        if (p->left == v) //указатель у родителя меняем на NULL
+            p->left = NULL;
+        if (p->right == v)
+            p->right = NULL;
+        delete v;
+    }
+    else if (!v->left || !v->right){//если только один ребенок
+        if (!p) { //если удаляем корень, у которого 1 ребенок
+            if (!v->left){ //если есть правый ребенок
+                tr = v->right; //он становится корнем
+                v->parent = NULL;
+            }
+            else { //аналогично для левого
+                tr=v->left;
+                v->parent=NULL;
+            }
+        }
+        else{
+            if(!v->left){//если есть правый ребенок
+                if(p->left==v)//если удаляемый узел явл. левым ребенком
+                    p->left=v->right;//ребенок удаляемого узла становится левым ребенком своего "деда"
+                else
+                    p->right=v->right;////ребенок удаляемого узла становится правым ребенком своего "деда"
+            v->right->parent=p;//родителем ребенка становится его "дед"
+            }
+            else{//аналогично для левого ребенка
+                if(p->left==v)
+                    p->left=v->left;
+                else
+                    p->right=v->left;
+                v->left->parent=p;
+            }
+            delete v;
+        }
+    }
+    else {//есть оба ребенка
+        tree *succ=Next(tr,v->inf);//следующий за удаляемым узлом
+        v->inf=succ->inf;//присваиваем значение
+        if(succ->parent->left==succ){//если succ левый ребенок
+            succ->parent->left=succ->right;//его правый ребенок становится левым ребенком своего "деда"
+            if(succ->right)//если этот ребенок существует
+                succ->right->parent=succ->parent;//его родителем становится "дед"
+        }
+        else{//аналогично если succ - правый ребенок
+        succ->parent->right=succ->right;
+        if(succ->right)
+            succ->right->parent=succ->parent;
+        }
+        delete succ;
+    }
+
 }
 
 int main(){
